73a1ebfb69b4fb39220fcdf96b57375aa48399d2
Ruddy Johns <ruddujohnstankeu@cmail.carleton.ca>
2023-11-26 20:12:21 -0500

    Add Changes

--- script-01.js
+++ script-01.js
@@ -55,14 +55,48 @@ const pokemons = [
 
 // 1. Display the Pokemons that have a level between 30 and 50
 
+for (let i = 0; i < pokemons.length; i++) {
+  if (pokemons[i].level >= 30 && pokemons[i].level <= 50) {
+    console.log(pokemons[i].name);
+  }
+}
 
 // 2. Display the pokemons that have an ability called "Run Away"
 
+console.log("");
+
+for (let i = 0; i < pokemons.length; i++) {
+  for (let j = 0; j < pokemons[i].abilities.length; j++) {
+    if (pokemons[i].abilities[j] == "Run Away") {
+      console.log(pokemons[i].name);
+    }
+  }
+}
 
 // 3. Display how many Pokemons have the type "Normal"
 
+console.log("");
+
+let count = 0;
+
+for (let i = 0; i < pokemons.length; i++) {
+  if (pokemons[i].type == "Normal") {
+    count++
+  }
+}
+
+console.log(count);
 
 // 4. Display the names of all of pokemons that have a type of "Psychic"
+
+console.log("");
+
+for (let i = 0; i < pokemons.length; i++) {
+  if (pokemons[i].type == "Psychic") {
+    console.log(pokemons[i].name);
+  }
+}
+
 
 
 // 5. Display the types of Pokemon and the number of Pokemons that have that type
@@ -74,4 +108,28 @@ const pokemons = [
 // eg: { type: 'Normal', count: 3 }
 
 
+console.log("");
+
+let typesCount = [];
+
+for (let i = 0; i < pokemons.length; i++) {
+  let type = pokemons[i].type;
+  let typeExists = false;
+
+  for (let j = 0; j < typesCount.length; j++) {
+    if (typesCount[j].type === type) {
+      typeExists = true;
+      typesCount[j].count++;
+    }
+  }
+
+  if (!typeExists) {
+    typesCount.push({ type: type, count: 1 });
+  }
+}
+
 // Display the results
+
+for (let i = 0; i < typesCount.length; i++) {
+  console.log(`${typesCount[i].type} - ${typesCount[i].count}`);
+}
--- script-03.js
+++ script-03.js
@@ -26,11 +26,11 @@
 // 1. Array of pokemons that are provided by the user
 const pokemons = [];
 // 2. The ul for the list of pokemon
-
+const pokeList = document.querySelector(".poke-items");
 // 3. The form where we intake pokemon
-
+const pokeForm = document.querySelector(".add-pokemon");
 // 4. The form text element that has the name the user provided
-
+const pokeName = document.querySelector("#pokemon-name");
 
 //
 // FUNCTIONS
@@ -39,13 +39,13 @@ const pokemons = [];
 // 5. Handle the event when a user submits the form
 function addPokemonItem(event) {
   // Stop browser default form submission
-
+  event.preventDefault();
   // Get the text from the input field
-
+  const pokemonName = pokeName.value;
   // Add the user defined pokemon to our array
-
+  pokemons.push(pokemonName);
   // Draw the list of pokemons using the renderList function
-
+  renderList();
   // Reset the form so that the text field name is cleared
   pokeForm.reset();
 }
@@ -59,10 +59,22 @@ function addPokemonItem(event) {
 function renderList() {
   // Clear all of the entries in the list
   // pokeList
+  while (pokeList.firstChild) {
+    pokeList.removeChild(pokeList.firstChild);
+  }
 
   // For each item in the list add a list item
   // pokemons[]
+  for (let i = 0; i < pokemons.length; i++) {
+    const listItem = document.createElement("li");
+    listItem.textContent = pokemons[i];
 
+    if (i === pokemons.length - 1) {
+      listItem.classList.add("new-item-annimate");
+    }
+
+    pokeList.appendChild(listItem);
+  }
 
 }
 
